b13f95e1db1fd8b7831c01d474166976199a6d79
JustMatt <MatthewBertrandt@gmail.com>
2018-05-14 06:43:45 -0700

    Tidied up game.js and moved <script> after body in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,62 @@
-//Variables
-var wins = 0, losses = 0, guessesLeft = 5;
+//Constant// Sets the max amount of guesses the player has
+const maxGuesses = 4;
+
+//Variables// Wins and losses are purely visual and serve no purpose as a variable
+var guessesLeft = maxGuesses;
 var guessesList = [];
-var guess = "", answer = "";
+var answer = "";
 
-//Functions
-function generateRandomLetter() {
-    answer = String.fromCharCode(Math.floor(Math.random() * 26) + 1 + 97);
-    console.log("The Answer: ", answer);
-}
-
-function emptyGuessesList() {
-    document.getElementById("guessList").innerHTML = "";
-    guessesList = [];
-}
-document.onkeydown = function() {
 //Variable-To-ID-Links
 var winsID = document.getElementById("wins");
 var lossesID = document.getElementById("losses");
 var guessesID = document.getElementById("guesses");
 var guessesListID = document.getElementById("guessList");
 
+
+//Functions// Generates a random number between 97 and 122, converts that into a char, and then returns it.
+function generateRandomLetter() { //ASCII Reference (https://www.w3schools.com/charsets/ref_html_ascii.asp)
+                                  //97 -> a    122 -> z
+    return String.fromCharCode(Math.floor((Math.random() * 26)) + 97);
+    //                                floor((0-.999) * 26) + 97
+    //                                floor(0-25.999) + 97 = 97 <-> 122
+}
+
+function testGenerateRandomLetter() { //Tests generateRandomLetter() and logs an array
+    var testList = [], testChar;//test variables
+    while(testList.length < 26){//generateRandomLetter() can only generate 26 different characters so 
+                                //this loop stops when the array is filled up with 26 unique entries
+        testChar = generateRandomLetter();
+        if (testList.indexOf(testChar) == -1){//Only pushes testChar if it does not exist in testList
+            testList.push(testChar);
+        }
+    }
+    console.log(testList.sort());
+}
+
+function emptyGuessesList() {
+    guessesList = [];
+    guessesListID.textContent = innerHTML = "";
+}
+
+document.onkeydown = function() {
     if (event.key >= 'a' && event.key <= 'z'){
         console.log(event.key);
         if (event.key == answer){
-            wins++;
-            winsID.textContent = wins;
-            generateRandomLetter();
+            winsID.textContent++;
+            answer = generateRandomLetter();
+            console.log("Answer: ", answer);
+            emptyGuessesList();
         }
         else {
             if(guessesList.indexOf(event.key) == -1){
-                guessesLeft--;
+                guessesID.textContent = --guessesLeft;
                 if (guessesLeft == 0){
-                    losses++;
-                    lossesID.textContent = losses;
-                    guessesLeft = 5;
-                    guessesID.textContent = guessesLeft;
+                    lossesID.textContent++;
+                    guessesLeft = maxGuesses;
+                    guessesID.textContent = maxGuesses;
+                    answer = generateRandomLetter();
+                    console.log("Answer: ", answer);
                     emptyGuessesList();
-                    generateRandomLetter();
                 }
                 else {
                     guessesList.push(event.key);
@@ -48,8 +68,11 @@ var guessesListID = document.getElementById("guessList");
     }
 }
 
-//The Rest
-generateRandomLetter();
+//On Startup
+guessesID.textContent = guessesLeft;
+answer = generateRandomLetter(); 
+console.log("Answer: ", answer);
+// testGenerateRandomLetter();
 
 
 
